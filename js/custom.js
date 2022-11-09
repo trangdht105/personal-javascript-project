2d66d1fdba6bbfc0389cfdfd712f394b4a9a7849
trangdht105 <108358250+trangdht105@users.noreply.github.com>
2022-11-09 16:58:59 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Marketer.", "Graphic Designer.","UX/UI Designer."],
+       	 	typeSpeed:100,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+
+new WOW().init();
---
+++ js/owl-custom.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+$('#slider').owlCarousel({
+loop:true,	// cho lap lại
+items: 1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	dots: false,
+})
+
+
+$('#slider-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
